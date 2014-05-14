1a73fbf80a55aae3a9de4e48383f0f134d380fb4
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-05-14 18:24:09 -0400

    Added some notes

--- lib/facter/drupalsiteinstalled.rb
+++ lib/facter/drupalsiteinstalled.rb
@@ -1,6 +1,8 @@
 # @todo change to check drupal site install, not drush
 Facter.add('drupalsiteinstalled') do
   setcode do
+    # @todo perhaps use drush @alias status?
+    # or drush --root='/var/www/html/drupal' --uri='https://example.com' status or something
     if File.exist? "/usr/local/bin/drush"
       'installed'
     else
