f5b6290381dacf097c939832f2fa76a25532fe47
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-05-14 18:35:58 -0400

    Switched to underscores

--- manifests/distro.pp
+++ manifests/distro.pp
@@ -1,6 +1,8 @@
 # Defines a drupalsi::distro resource
 
-define drupalsi::distro ($distro_settings) {
+define drupalsi::distro ($distribution = 'drupal', $core = '7.x', $version = 'latest', $build_location = 'https://updates.drupal.org/release-history', $build_type = 'get', $site_root = '/var/www/html/drupal', $build_args = '') {
   include drush
 
+  $profiles = hiera_hash("drupalsi::${distribution}::profiles")
+  create_resource('drupalsi::profile', $profiles)
 }
