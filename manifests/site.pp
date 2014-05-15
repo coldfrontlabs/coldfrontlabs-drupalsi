4d2121c0daba50f6631a0b32536c1bc36fe02eb4
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-05-15 18:04:32 -0400

    Added signature for drush si

--- manifests/site.pp
+++ manifests/site.pp
@@ -18,6 +18,16 @@ define drupalsi::site ($profile,
 ) {
   include drush
 
+
+  drush::si {"drush-si-${name}-${site_name}-${base_url}":
+    source => $distro_build_location,
+    destination => $distro_root,
+    project_name => $distribution,
+    default_major => $api_version,
+    drupal_project_rename => $name,
+    onlyif => "test ! -f ${distro_root}/${name}/index.php",
+  }
+
   # Ex: drush si --root=${distribution}::site_root --destination=${site}::sites_subdir --db-url=${sites}::db_url ...
   # Also need the distro_root value from the distro the site is being installed and set the --root option for drush
 }
