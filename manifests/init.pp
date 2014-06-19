105f57362d7598e1f841043cde22311e145d9457
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-06-19 18:27:26 -0400

    Added init for site aliases

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,4 +11,7 @@ class drupalsi () {
 
   $sites = hiera_hash('drupalsi::sites', {})
   create_resources(drupalsi::site, $sites)
+
+  $site_aliases = hiera_hash('drupalsi::aliases', {})
+  create_resources(drupalsi::site_alias_file, $site_aliases)
 }
