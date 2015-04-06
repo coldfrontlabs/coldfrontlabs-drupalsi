899d21fb841a27ea5225a93b3f5577293343ec35
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2015-04-06 13:16:16 -0400

    fix(validation): Use validation properly

--- manifests/distro.pp
+++ manifests/distro.pp
@@ -96,11 +96,10 @@ define drupalsi::distro::omitfiles() {
   # Since I can't loop or pass other arguments, we have to build data into the string
   # Not ideal but it works. If anyone has a better idea please submit a patch
   $parts = split($name, "||")
+  validate_absolute_path($parts[1])
 
-  if validate_absolute_path($parts[1]) {
-    file{$parts[1]:
-      ensure => 'absent',
-      require => $parts[0],
-    }
+  file{$parts[1]:
+    ensure => 'absent',
+    require => $parts[0],
   }
 }
