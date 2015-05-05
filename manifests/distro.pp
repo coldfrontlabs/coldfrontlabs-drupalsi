b054c24886639874bfc52c7757c0977099c536b2
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2015-05-05 07:55:24 -0400

    restore escape

--- manifests/distro.pp
+++ manifests/distro.pp
@@ -105,7 +105,7 @@ define drupalsi::distro ($distribution = 'drupal',
 define drupalsi::distro::omitfiles() {
   # Since I can't loop or pass other arguments, we have to build data into the string
   # Not ideal but it works. If anyone has a better idea please submit a patch
-  $parts = split($name, "||")
+  $parts = split($name, "\|\|")
 
   notify {"Removing file ${parts[1]} from Drupal distribution.":}
 
