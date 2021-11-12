759c4860c8438f69f548b049258c7f6f288e4379
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2021-11-12 16:21:45 -0500

    Update distro.pp

--- manifests/distro.pp
+++ manifests/distro.pp
@@ -1,11 +1,11 @@
 # Defines a drupalsi::distro resource
 define drupalsi::distro ($distribution = 'drupal',
                          $api_version = 8,
-                         $distro_root = '/var/www/html',
+                         $distro_root = '/var/www/html/drupal',
                          $distro_build_type = 'composer',
                          $distro_build_location = 'https://updates.drupal.org/release-history', # deprecated.
                          $distro_build_args = {},
-                         $omit_files = {}
+                         $omit_files = {} #deprecated.
                          )
 {
   include ::drush
@@ -24,11 +24,11 @@ define drupalsi::distro ($distribution = 'drupal',
        command => "composer create-project drupal/recommended-project ${name} -y",
        cwd => $distro_root,
        path => ['/usr/local/bin', '/usr/bin'],
-       creates => "${distro_root}/${name}/index.php",
+       creates => $distro_root,
     }
     exec {"composer-require-drush-${buildname}":
       command => "composer require drush/drush",
-      cwd => "${distro_root}/${name}",
+      cwd => $distro_root,
        path => ['/usr/local/bin', '/usr/bin'],
        subscribe   => Exec["composer-install-drupal-${buildname}"],
        refreshonly => true,
@@ -45,40 +45,20 @@ define drupalsi::distro ($distribution = 'drupal',
     # @todo change to use vcsrepo puppet module.
     exec {"git-clone-${buildname}":
       command => "git clone ${branch} ${distro_build_location} ${distro_root}/${name}",
-      creates => "${distro_root}/${name}/index.php",
+      creates => $distro_root,
       path => ["/usr/bin", "/usr/sbin"],
       require => Class['git'],
-      onlyif => "test ! -d ${distro_root}/${name}",
+      onlyif => "test ! -d ${distro_root}",
       timeout => 1800,
     }
 
     $buildaction = "Exec[git-clone-${buildname}]"
 
     # Ensure the file is there even if it's blank.
-    file {"${distro_root}/${name}/sites/sites.php":
+    file {"${distro_root}/sites/sites.php":
       ensure => 'present',
       require => Exec["git-clone-${buildname}"],
       mode => '0644',
     }
-  }
-
-  if !empty($omit_files) {
-    # See below why we're doing this
-    $omitfiles = prefix($omit_files, "${buildaction}||${distro_root}/${name}/")
-    drupalsi::distro::omitfiles{$omitfiles:}
-  }
-}
-
-# Remove files
-define drupalsi::distro::omitfiles() {
-  # Since I can't loop or pass other arguments, we have to build data into the string
-  # Not ideal but it works. If anyone has a better idea please submit a patch
-  $parts = split($name, "\|\|")
-
-  validate_absolute_path($parts[1])
-
-  file{$parts[1]:
-    ensure => 'absent',
-    require => $parts[0],
   }
 }
