7728d1744c8656234f9b83dfc7307062e3304a00
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2023-02-06 14:02:44 -0500

    Composer initial test (#86)
    * feat(install): add initial install for composer projects
    
    * refactor(distro): remove deprecated install methods
    
    Drop drush make
    Drop wget
    Drop archive restore
    
    * refactor(install): remove install profiles
    
    Git and composer take those into account automatically
    
    * refactor(install): remove install profiles
    
    Not required for composer or git installs
    
    * Delete drush.make.erb
    
    * Update htaccess-public.erb
    
    * Update htaccess-private.erb
    
    * Update distro.pp
    
    * Update distro.pp
    
    * Update init.pp
    
    * feat(composer): run as non-root
    
    * feat(composer): run as non-root
    
    * feat(composer): run as non-root
    
    * refactor(puppet): use stdlib ensure_resources
    
    * fix(type): ensure integer comparison
    
    * feat(dotenv): manage a distro wide env file
    
    * feat(vars): add site specific env vars
    
    * fix(ownership): use web_user
    
    Maybe deprecate $owner variable?
    
    * feat(settings): populate default settings
    
    Eventually we'll set to replace
    
    * fix(composer): require the PHP class first
    
    * fix(composer): require the PHP class first
    
    * Update distro.pp
    
    * fix(composer): check for composer.json file
    
    * refactor(composer): cleanup the env file prior to initial isntall
    
    * fix(clean): add arg
    
    * fix(clean): add arg
    
    * refactor(composer): clear .env
    
    Do so as part of the composer create
    
    Can't get puppet to do things in the right order :-/
    
    * refactor(composer): clear .env
    
    Do so as part of the composer create
    
    Can't get puppet to do things in the right order :-/
    
    * refactor(composer): clear .env
    
    Do so as part of the composer create
    
    Can't get puppet to do things in the right order :-/
    
    * refactor(composer): clear .env
    
    Do so as part of the composer create
    
    Can't get puppet to do things in the right order :-/
    
    * fix(order): only create the env if installed
    
    * refactor(distro): use owner
    
    * refactor(distro): use owner
    
    * refactor(distro): use owner
    
    * refactor(composer): reorder items
    
    * refactor(vars): use env

--- manifests/distro.pp
+++ manifests/distro.pp
@@ -1,18 +1,17 @@
 # Defines a drupalsi::distro resource
 define drupalsi::distro (
   $distribution = 'drupal',
-  $api_version = 7,
-  $distro_root = '/var/www/html',
+  $env = {},
+  $api_version = 8,
+  $distro_root = '/var/www/html/drupal',
   $distro_docroot = 'web',
-  $distro_build_type = 'get',
-  $distro_build_location = 'https://updates.drupal.org/release-history',
+  $distro_build_type = 'composer',
+  $distro_build_location = 'https://updates.drupal.org/release-history', # deprecated.
   $distro_build_args = {},
-  $omit_files = {},
-  $env = {}
-)
-{
-
-  include drush
+  $omit_files = {}, #deprecated
+  $owner = 'apache',
+) {
+  include ::drush
 
   if  $::osfamily == 'RedHat' {
     $web_user = 'apache'
@@ -31,37 +30,93 @@ define drupalsi::distro (
   if ($distro_build_type == 'composer' or Integer($api_version) >= 8) {
     # Do nothing for now.
     # @todo run composer install or just leave it be?
-
-    exec {"create-${buildname}-sites.php":
-      creates => "${distro_root}/${distro_docroot}/sites/sites.php",
-      command => "/bin/cp ${distro_root}/${distro_docroot}/sites/example.sites.php ${distro_root}/${distro_docroot}/sites/sites.php"
+    exec {"composer-require-cleanenv-${buildname}":
+      command     => 'rm -f .env',
+      cwd         => $distro_root,
+      path        => ['/usr/local/bin', '/usr/bin', '/bin'],
+      creates     => "${distro_root}/composer.json",
+      environment => ['HOME=/var/www'],
+    }
+    ~> exec {"composer-install-drupal-${buildname}":
+      command     => "composer create-project  --remove-vcs --no-cache --no-interaction drupal/recommended-project ${distro_root}",
+      path        => ['/usr/local/bin', '/usr/bin'],
+      creates     => "${distro_root}/composer.json",
+      user        => $owner,
+      environment => ['HOME=/var/www'],
+      require     => Class['php'],
     }
 
-    concat {"${distro_root}/.env":
-      ensure_newline => true,
-      replace        => false,
-      backup         => false,
-      show_diff      => false,
-      group          => $web_user, # @todo use def modififier collector to fix this to webserver user.
+    exec {"composer-require-drush-${buildname}":
+      command     => 'composer require drush/drush',
+      cwd         => $distro_root,
+      path        => ['/usr/local/bin', '/usr/bin'],
+      subscribe   => Exec["composer-install-drupal-${buildname}"],
+      refreshonly => true,
+      user        => $owner,
+      environment => ['HOME=/var/www'],
     }
 
-    concat::fragment {"${buildname}-env":
-      target  => "${distro_root}/.env",
-      content => dotenv($env),
-      order   => '01'
+  }
+  elsif ($distro_build_type == 'git') {
+    include ::git
+    if has_key($distro_build_args, 'git_branch') {
+      $branch = join([ '-b ', '"', $distro_build_args['git_branch'], '"'])
+    } else {
+      $branch = ''
     }
 
-    concat {"${distro_root}/${distro_docroot}/sites.php":
-      ensure         => 'present',
-      path           => "${distro_root}/${distro_docroot}/sites/sites.php",
-      mode           => '0640',
-      ensure_newline => true,
-      replace        => false,
-      backup         => false,
-      show_diff      => true,
-      group          => $web_user, # @todo use def modififier collector to fix this to webserver user.
-      require        => Exec["create-${buildname}-sites.php"],
+    # @todo change to use vcsrepo puppet module.
+    exec {"git-clone-${buildname}":
+      command => "git clone ${branch} ${distro_build_location} ${distro_root}/${name}",
+      creates => $distro_root,
+      path    => ['/usr/bin', '/usr/sbin'],
+      require => Class['git'],
+      onlyif  => "test ! -d ${distro_root}",
+      timeout => 1800,
+    }
+
+    $buildaction = "Exec[git-clone-${buildname}]"
+
+    # Ensure the file is there even if it's blank.
+    file {"${distro_root}/sites/sites.php":
+      ensure  => 'present',
+      require => Exec["git-clone-${buildname}"],
+      mode    => '0644',
     }
   }
 
+  exec {"create-${buildname}-sites.php":
+    creates => "${distro_root}/${distro_docroot}/sites/sites.php",
+    command => "/bin/cp ${distro_root}/${distro_docroot}/sites/example.sites.php ${distro_root}/${distro_docroot}/sites/sites.php"
+  }
+
+  # Add an env file.
+  concat {"${distro_root}/.env":
+    ensure_newline => true,
+    replace        => false,
+    backup         => false,
+    show_diff      => false,
+    group          => $web_user, # @todo use def modififier collector to fix this to webserver user.
+    require        => Exec["composer-install-drupal-${buildname}"] #@todo generalize to work with git clone too.
+  }
+
+  # Add require env varialbes.
+  concat::fragment {"${distro_root}-envvars":
+    target  => "${distro_root}/.env",
+    content => dotenv($env),
+    order   => '01'
+  }
+
+  concat {"${distro_root}/${distro_docroot}/sites.php":
+    ensure         => 'present',
+    path           => "${distro_root}/${distro_docroot}/sites/sites.php",
+    mode           => '0640',
+    ensure_newline => true,
+    replace        => false,
+    backup         => false,
+    show_diff      => true,
+    group          => $web_user, # @todo use def modififier collector to fix this to webserver user.
+    require        => Exec["create-${buildname}-sites.php"],
+  }
 }
+
