919015d24f34c372dd45aa3582f6f74e6de19cd2
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2023-10-23 21:59:10 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -132,7 +132,6 @@ define drupalsi::site (
     backup         => false,
     show_diff      => false,
     group          => $webserver_user, # @todo use def modififier collector to fix this to webserver user.
-    notify         => Exec["distro-fix-perms-${distro}"],
   }
 
   # Populate the settings.php file with the default values.
@@ -159,7 +158,6 @@ define drupalsi::site (
     backup         => false,
     show_diff      => false,
     group          => $webserver_user, # @todo use def modififier collector to fix this to webserver user.
-    notify         => Exec["distro-fix-perms-${distro}"]
   }
 
   # Add entries into sites.php
