3cec3e0beabd8037352ff9925da148d0baedba84
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2024-05-01 11:22:53 -0400

    Drupalcomposerfix (#92)
    * fix composer initial install
    
    Require drush prior to 12 for now
    Set exec order
    
    * feat(perms): run fix perms
    
    * fix: set owner to cibuild by default
    
    Use the web user variable
    
    * Update distro.pp
    
    * fix(perms): add exec
    
    Reorder perm fixes
    Allow other items to call on a fix perm
    
    * fix(order): try requiring instance of defined type
    
    * Update site.pp
    
    * Update site.pp
    
    * Update site.pp
    
    * Update site.pp
    
    * Update site.pp
    
    * Update site.pp
    
    * Update site.pp

--- manifests/distro.pp
+++ manifests/distro.pp
@@ -107,6 +107,13 @@ define drupalsi::distro (
     require => File['drupal-fix-permissions-script']
   }
 
+  exec {"distro-fix-perms-${buildname}":
+    command => "/bin/sudo /usr/local/bin/drupal-fix-permissions.sh --drupal_user=${owner} --httpd_group=${web_user}",
+    cwd => "${distro_root}/${distro_docroot}",
+    refreshonly => true,
+    require => File['drupal-fix-permissions-script']
+  }
+
   # Add an env file.
   concat {"${distro_root}/.env":
     ensure_newline => true,
